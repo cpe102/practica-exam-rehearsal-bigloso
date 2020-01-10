36c970cd126ec381bb57858c53cd11160978c82d
Bigloso <wayside.big@gmail.com>
2020-01-10 17:36:58 +0700

    qwe

--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -27,7 +27,8 @@ int main()
     double e = sumSqrt(1);
     double f = sumSqrt(0);
     double g = sumSqrt(-1);
-    
-    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
+    double h = sumSqrt(-5);
+    double i = sumSqrt(3);
+    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n" << h <<"\n" << i << "\n";
 
 }
