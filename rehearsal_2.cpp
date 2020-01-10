212f353127a5e5195af689fc13f05a7ef4733e73
Bigloso <wayside.big@gmail.com>
2020-01-10 16:21:40 +0700

    AA

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <string>
+using namespace std ;
+int main(){
+    float sum = 0 ;
+    float x ;
+    cout << "Enter X:" ;
+    cin >> x ;
+    while (x!=0)
+    {
+        if (x>0)
+        {
+            sum = sum+x ;
+        }
+        cout << "Enter X:" ;
+    cin >> x ;
+    }
+    cout << "sum = "<< sum ;
+}
